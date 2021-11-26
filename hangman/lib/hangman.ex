6eb86b135aeeadf85cf1f4a5dfd8407b5b0acefb
pragdave <dave@pragdave.me>
2021-11-26 11:21:19 -0600

    complete 05-02

---
+++ hangman/lib/hangman.ex
@@ -0,0 +1,9 @@
+defmodule Hangman do
+
+  def new_game do
+  end
+
+  def make_move(game, guess) do
+  end
+
+end
---
+++ hangman/test/hangman_test.exs
@@ -0,0 +1,8 @@
+defmodule HangmanTest do
+  use ExUnit.Case
+  doctest Hangman
+
+  # test "greets the world" do
+  #   assert Hangman.hello() == :world
+  # end
+end
