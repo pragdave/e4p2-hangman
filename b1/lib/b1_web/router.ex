fef805f460cf4d0b126da635b70fc703ac7e0733
pragdave <dave@pragdave.me>
2021-11-27 15:11:30 -0600

    completed 17-06

--- b1/lib/b1_web/router.ex
+++ b1/lib/b1_web/router.ex
@@ -20,6 +20,8 @@ defmodule B1Web.Router do
     get  "/", HangmanController, :index
     post "/", HangmanController, :new
     put  "/", HangmanController, :update
+
+    get  "/current", HangmanController, :show
   end
 
   # Other scopes may use custom stacks.
--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,3 +1,100 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
+  
+  def figure_for(0) do
+    ~s{
+      ┌───┐
+      │   │
+      O   │
+     /|\\  │
+     / \\  │
+          │
+    ══════╧══
+    }
+  end
+  def figure_for(1) do
+    ~s{
+      ┌───┐
+      │   │
+      O   │
+     /|\\  │
+     /    │
+          │
+    ══════╧══
+    }
+  end
+
+  def figure_for(2) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+   /|\\  │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(3) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+   /|   │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(4) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+    |   │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(5) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(6) do
+    ~s{
+    ┌───┐
+    │   │
+        │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(7) do
+    ~s{
+    ┌───┐
+        │
+        │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
+
+
 end
