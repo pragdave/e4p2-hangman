e9d3167f927cfc8b25f93690733bcdc737772b6c
pragdave <dave@pragdave.me>
2021-11-27 16:14:03 -0600

    complete 19-02

--- b2/lib/b2_web/live/game.ex
+++ b2/lib/b2_web/live/game.ex
@@ -9,9 +9,14 @@ defmodule B2Web.Live.Game do
     { :ok, socket }
   end
 
+  def handle_event("make_move", %{ "key" => key }, socket) do
+    tally = Hangman.make_move(socket.assigns.game, key)
+    { :noreply, assign(socket, :tally, tally) }
+  end
+  
   def render(assigns) do
     ~L"""
-    <div class="game-holder">
+    <div class="game-holder" phx-window-keyup="make_move">
       <%= live_component(__MODULE__.Figure,    tally: assigns.tally, id: 1 )%> 
       <%= live_component(__MODULE__.Alphabet,  tally: assigns.tally, id: 2 )%> 
       <%= live_component(__MODULE__.WordSoFar, tally: assigns.tally, id: 3 )%> 
--- b2/lib/b2_web/live/game/word_so_far.ex
+++ b2/lib/b2_web/live/game/word_so_far.ex
@@ -2,13 +2,37 @@ defmodule B2Web.Live.Game.WordSoFar do
 
   use B2Web, :live_component
 
+  @states %{
+    already_used: "You already picked that letter",
+    bad_guess:    "That's not in the word",
+    good_guess:   "Good guess!",
+    initializing: "Type or click on your first guess",
+    lost:         "Sorry, you lost...",
+    won:          "You won!",
+  }
+
   def mount(socket) do
-    { :ok, socket }
+    {:ok, socket}
+  end
+
+  defp state_name(state) do
+    @states[state] || "Unknown state"
   end
 
   def render(assigns) do
     ~L"""
-    In igure
+    <div class="word-so-far">
+      <div class="game-state">
+         <%= state_name(@tally.game_state) %>
+    </div>
+    <div class="letters">
+    <%= for ch <- @tally.letters do %>
+      <div class="one-letter <%= if ch != "_", do: "correct" %>">
+        <%= ch %>
+      </div>
+    <% end %>
+    </div>
+    </div>
     """
   end
 end
