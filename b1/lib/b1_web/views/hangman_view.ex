e3780d66726baec2d7a5dbff1c7af2b9daae03fa
pragdave <dave@pragdave.me>
2021-11-27 15:23:32 -0600

    complete 17-07

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,6 +1,34 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
-  
+
+  def continue_or_try_again(conn, status) when status in [ :won, :lost ] do
+    button("Try again", to: Routes.hangman_path(conn, :new))
+      end
+
+  def continue_or_try_again(conn, _) do
+     form_for(conn, Routes.hangman_path(conn,  :update), [ as: "make_move", method: :put ], fn f -> 
+       [
+         text_input(f, :guess),
+         " ",
+         submit("Make next guess"),
+       ]
+    end) 
+  end
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter a a time" },
+    good_guess:   {"good-guess",    "Good guess!"},
+    bad_guess:    {"bad-guess",     "Sorry, that's a bad guess"},
+    won:          {"won",           "You won!"},
+    lost:         {"lost",          "Sorry, you lost"},
+    already_used: {"already-used",  "You already used that letter"}
+  }
+
+  def move_status(status) do  
+    { class, msg } = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
   def figure_for(0) do
     ~s{
       ┌───┐
